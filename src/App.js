fc965704c31ca80c858caf6a986b5b67c349ab04
Dao Minh Khoi <khoidm.bhsoft@gmail.com>
2020-06-10 10:07:31 +0700

    commit lai file App

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Canvas } from 'react-three-fiber';
 import Box from './component/box';
 import Controls from './component/controls';
 import Plane from './component/plane';
-import Model from './component/model';
+// import Model from './component/model';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<Canvas>
-				<Model />
+				{/* <Model /> */}
 				<fog attach="fog" args={['white', 10, 25]} />
 				<ambientLight />
 				<spotLight position={[15, 20, 5]} penumbra={1} castShadow />
